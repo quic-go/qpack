9e159b6984cca28f0902fa582dc698161c00dc7c
Marten Seemann <martenseemann@gmail.com>
2019-03-02 09:47:30 +0900

    implement decoding of literal header fields without name reference

--- decoder.go
+++ decoder.go
@@ -73,10 +73,12 @@ func (d *Decoder) decode() error {
 		b := d.buf[0]
 		var err error
 		switch {
-		case b&0x80 > 0:
+		case b&0x80 > 0: // 1xxxxxxx
 			err = d.parseIndexedHeaderField()
-		case b&0xc0 == 0x40:
+		case b&0xc0 == 0x40: // 01xxxxxx
 			err = d.parseLiteralHeaderField()
+		case b&0xe0 == 0x20: // 001xxxxx
+			err = d.parseLiteralHeaderFieldWithoutNameReference()
 		default:
 			err = fmt.Errorf("unexpected type byte: %#x", d.buf[0])
 		}
@@ -105,8 +107,7 @@ func (d *Decoder) parseIndexedHeaderField() error {
 }
 
 func (d *Decoder) parseLiteralHeaderField() error {
-	b := d.buf
-	if b[0]&0x20 > 0 || b[0]&0x10 == 0 {
+	if d.buf[0]&0x20 > 0 || d.buf[0]&0x10 == 0 {
 		return errNoDynamicTable
 	}
 	index, rest, err := readVarInt(4, d.buf)
@@ -124,23 +125,56 @@ func (d *Decoder) parseLiteralHeaderField() error {
 		return err
 	}
 	d.buf = rest
+	val, err := d.parseString(l, usesHuffman)
+	if err != nil {
+		return err
+	}
+	hf.Value = val
+	d.emitFunc(hf)
+	return nil
+}
+
+func (d *Decoder) parseLiteralHeaderFieldWithoutNameReference() error {
+	usesHuffmanForName := d.buf[0]&0x8 > 0
+	nameLen, rest, err := readVarInt(3, d.buf)
+	if err != nil {
+		return err
+	}
+	d.buf = rest
+	name, err := d.parseString(nameLen, usesHuffmanForName)
+	if err != nil {
+		return err
+	}
+	usesHuffmanForVal := d.buf[0]&0x80 > 0
+	valLen, rest, err := readVarInt(7, d.buf)
+	if err != nil {
+		return err
+	}
+	d.buf = rest
+	val, err := d.parseString(valLen, usesHuffmanForVal)
+	if err != nil {
+		return err
+	}
+	d.emitFunc(HeaderField{Name: name, Value: val})
+	return nil
+}
+
+func (d *Decoder) parseString(l uint64, usesHuffman bool) (string, error) {
 	if uint64(len(d.buf)) < l {
-		return errors.New("too little data")
+		return "", errors.New("too little data")
 	}
 	var val string
 	if usesHuffman {
 		var err error
 		val, err = hpack.HuffmanDecodeToString(d.buf[:l])
 		if err != nil {
-			return err
+			return "", err
 		}
 	} else {
 		val = string(d.buf[:l])
 	}
 	d.buf = d.buf[l:]
-	hf.Value = val
-	d.emitFunc(hf)
-	return nil
+	return val, nil
 }
 
 func (d *Decoder) at(i uint64) (hf HeaderField, ok bool) {
